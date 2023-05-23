36ad9514b0ce3f8a9fcb62a9e29a201f55ac4b26
sammyredha <sammy.mounsif@melroseschools.com>
2023-05-23 19:58:39 -0400

    check if robot is real before trying to log to usb

--- src/main/java/frc/robot/GVersion.java
+++ src/main/java/frc/robot/GVersion.java
@@ -5,13 +5,13 @@ public final class GVersion {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Senioritis";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "962fd1fc959c0bdb0b9c2c19a1b59ce62d91ab5c";
-  public static final String GIT_DATE = "2023-05-23 12:01:50 EDT";
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "938833b7da477a25be41042e1a14e283e9414529";
+  public static final String GIT_DATE = "2023-05-23 19:37:01 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2023-05-23 19:35:32 EDT";
-  public static final long BUILD_UNIX_TIME = 1684884932269L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2023-05-23 19:48:48 EDT";
+  public static final long BUILD_UNIX_TIME = 1684885728116L;
+  public static final int DIRTY = 0;
 
   private GVersion() {}
 }
