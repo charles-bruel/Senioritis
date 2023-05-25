4b0421e443fbc9229371002a31b306e397ada643
sammyredha <sammy.mounsif@melroseschools.com>
2023-05-24 20:38:12 -0400

    define aziAbs.Enc.Raw Deg.
    Co-Authored-By: Aleksandr Usanov <60308780+phalanx5563@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/Chassis/Modules/ModuleIO.java
+++ src/main/java/frc/robot/subsystems/Chassis/Modules/ModuleIO.java
@@ -7,6 +7,7 @@ public interface ModuleIO {
   @AutoLog
   public class ModuleInputs {
     public double aziAbsoluteEncoderDegrees = 0.0;
+    public double aziAbsoluteEncoderRawDegrees = 0.0;
     public double aziOutputVolts = 0.0;
     public double aziCurrentDrawAmps = 0.0;
     public double aziEncoderPositionDeg = 0.0;
